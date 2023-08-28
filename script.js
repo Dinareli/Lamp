d70f3380deafadd95a1f8a47d279e1468e59f5ad
Natalia Dinareli <dinareli.lima@gmail.com>
2023-08-27 21:38:29 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const lampImage = document.getElementById('lampImage');
+
+function ligarLampada() {
+  lampImage.src = 'lamp_on.jpg';
+}
+
+function desligarLampada() {
+  lampImage.src = 'lamp_off.jpg';
+}
